dd11283d9f90faa1f746803c8a772b31391a589f
David W <111401603+sciwalk@users.noreply.github.com>
2024-10-18 05:02:22 -0700

    Add files via upload

---
+++ js/01-coinflip-redux.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 5; i++) {
+    let coinFlip = Math.round(Math.random())
+
+    if (!coinFlip) {
+        console.log('Heads')
+
+    } else {
+        console.log('Tails')
+
+    }
+}
