5259a2dc77c90736238e3c929cf74afb43a658fe
David W <111401603+sciwalk@users.noreply.github.com>
2024-10-18 05:20:15 -0700

    Update 02-coinflip-streak.js

---
+++ js/02-coinflip-streak.js
@@ -0,0 +1,9 @@
+let coinFlip
+do {
+    coinFlip = Math.round(Math.random())
+    if (coinFlip) {
+        console.log('Heads')
+    } else {
+        console.log('Tails')
+    }
+} while (coinFlip)
