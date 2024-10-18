4ac332c602811067660b0dce1b3883e608785985
David W <111401603+sciwalk@users.noreply.github.com>
2024-10-18 05:41:09 -0700

    Update 01-coinflip-redux.js

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -1,11 +1,9 @@
-for (let i = 1; i <= 5; i++) {
+const numberOfFlips = prompt("Please enter the number of coinflips", 10)
+for (let i = 1; i <= numberOfFlips; i++) {
     let coinFlip = Math.round(Math.random())
-
     if (!coinFlip) {
         console.log('Heads')
-
     } else {
         console.log('Tails')
-
     }
 }
